2736645d93bf0390fe105f1fc5b23357a6e2d936
Juan Aguilera <j.aguilerahuerta@ugto.mx>
2022-09-07 12:14:57 -0500

    Loading and drawing textures in Quads

--- OpenGLFun/OpenGLFun/OpenGLFun.cpp
+++ OpenGLFun/OpenGLFun/OpenGLFun.cpp
@@ -1,10 +1,52 @@
 #include <xstddef>
 #include <windows.h>
 #include "glut.h">
+#include <cstdio>
+
+GLuint texture;
+#pragma warning(disable: 4996)
+
+bool loadTexture(const char* filename)
+{
+	unsigned char header[54];
+	unsigned char* data;
+	int dataPos;
+	int width;
+	int height;
+	int imageSize;
+
+	FILE* file = fopen(filename, "rb");
+	if (!file) return false;
+	if (fread(header, 1, 54, file) != 54) return false;
+	if (header[0] != 'B' || header[1] != 'M') return false;
+
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+
+	if (imageSize == 0) imageSize = width * height * 3;
+	if (dataPos == 0) dataPos = 54;
+
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+	fclose(file);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	
+	return true;
+}
+
 
 void initGL() 
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 }
 
 void drawPoints()
@@ -69,13 +111,27 @@ void drawQuad()
 	//test
 	glEnd();
 }
+
+void drawTexture()
+{
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glBegin(GL_QUADS);
+	glTexCoord2d(0.0, 0.0); glVertex2d(0.0, 0.0);
+	glTexCoord2d(1.0, 0.0); glVertex2d(0.5, 0.0);
+	glTexCoord2d(1.0, 1.0); glVertex2d(0.5, 0.5);
+	glTexCoord2d(0.0, 1.0); glVertex2d(0.0, 0.5);
+
+	glEnd();
+}
+
 void update()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	//	drawPoints();
 	//	drawLines();
 	//	drawSolidTriangle();
-	drawQuad();
+	//drawQuad();
+	drawTexture();
 	glFlush();
 }
 
@@ -86,6 +142,7 @@ int main()
 	glutInitWindowPosition(500, 500);
 	glutDisplayFunc(update);
 	initGL();
+	//loadTexture("*/estesi.bmp"); 
 	glutMainLoop();
 	return 0;
 }
